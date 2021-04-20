a9022f8f9967ee5ba78230d437549741de3acbd1
Angela <angelaj4@illinois.edu>
2021-04-20 17:14:47 -0500

    HELP

---
+++ parse.cpp
@@ -0,0 +1,24 @@
+#include "parse.h"
+#include <fstream>
+#include <sstream>
+
+
+	
+	
+namespace parse {
+	
+std::vector<std::string> file_to_vector(const std::string & file) {
+	std::vector<std::string> result;
+    std::ifstream csv(file);
+    
+    std::istream_iterator<std::string> eos;
+    std::istream_iterator<std::string> iter(csv);
+    while (iter != eos) {
+        result.push_back(*iter);
+        ++iter;        
+	}	
+    return result;
+	
+}
+	
+	}
---
+++ parse.h
@@ -0,0 +1,13 @@
+#pragma once
+	#include <vector>
+	#include <cstdlib>
+	#include <fstream>
+	#include <iostream>
+	#include <string>
+	
+	namespace parse {
+	
+	std::vector<std::string> file_to_vector(const std::string &file);
+	
+	
+	}
