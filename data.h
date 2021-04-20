37d95cccbde3e4ebe41e04ee76fb98a8b74ded65
Jared Frank <jafrank2@illinois.edu>
2021-04-19 21:57:48 -0500

    updated header file

--- data.h
+++ data.h
@@ -1,24 +1,59 @@
 #pragma once
 #include <_stdio.h>
 #include <vector>
+#include <string>
 
-class Data {
+class AirTravel {
     public:
 
-    struct Pairs {
-        char other_airport;
-        int distance;
-    };
+    /**
+     * Constructor
+     */
+    AirTravel();
 
-    struct Node {
-        Node();
-        char name;
-        std::vector<Pairs> destinations;
-        int latitude;
-        int longitude;
-    };
+    /**
+     * Constructor with parameters
+     * 
+     * @param Airport_File name of csv file containing airport data 
+     * @param Routes_File name of csv file containing routes data
+     */
+    AirTravel(std::string Airport_File, std::string Routes_File);
+
+    /**
+     * Copy Contructor
+     */
+    AirTravel(AirTravel& other);
+
+    /**
+     * Destructor
+     */
+    ~AirTravel();
+
+    /**
+     * Copy Assignment Constructor
+     */
+    AirTravel& operator=(const AirTravel& other);
 
     private:
-    
 
+    /**
+     * Weighted edges for our graph
+     * 
+     * Takes into account one way flights!!
+     */
+    struct Flights {
+        char other_airport; //connected node
+        int distance; //weight
+    };
+
+    /**
+     * Nodes for our graph
+     */
+    struct Airport {
+        std::string name;
+        std::vector<Flights> destinations;
+        double latitude;
+        double longitude;
+    };
+    
 };
