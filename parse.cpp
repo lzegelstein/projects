e8f50d0f167ec49b5c47740469047842cd403177
Angela <angelaj4@illinois.edu>
2021-04-20 19:50:28 -0500

    parse is not done

--- parse.cpp
+++ parse.cpp
@@ -2,17 +2,36 @@
 #include <fstream>
 #include <sstream>
 	
-namespace parse {	
-    std::vector<std::string> file_to_vector(const std::string & file) {
-	    std::vector<std::string> result;
-        std::ifstream csv(file);
-    
-        std::istream_iterator<std::string> eos;
-        std::istream_iterator<std::string> iter(csv);
-        while (iter != eos) {
-            result.push_back(*iter);
-            ++iter;        
-	    }	
-        return result;
-    }
+namespace parse {
+	
+std::vector<std::string> file_to_vector(const std::string & file) {
+	std::vector<std::string> result;
+    std::ifstream csv(file);
+    std::istream_iterator<std::string> iter(csv);
+    std::istream_iterator<std::string> datafile;
+	while(iter != datafile) {
+        result.push_back(*iter);
+        iter++;        
+	}
+    return result;
+}
+
+std::vector<std::vector<std::string>> tokenizer(const std::vector<std::string> & csv) {
+    std::vector<std::vector<std::string>> result;
+
+	return result;
+
+}
+std::vector<std::string> helper(std::string& toke_help, const char& comma) {
+	std::vector<std::string> result;
+	std::string line;
+	std::stringstream stream(line);
+	std::string token;
+	while(std::getline(stream, token, ',')){
+		result.push_back(token);
+	}
+	return result;
+}
+
+
 }
--- parse.h
+++ parse.h
@@ -9,5 +9,7 @@
 	
 	std::vector<std::string> file_to_vector(const std::string &file);
 	
-	
+	std::vector<std::vector<std::string>> tokenizer(const std::vector<std::string> & csv);
+
+	std::vector<std::string> helper(std::string& token_help, const char& comma);
 	}
