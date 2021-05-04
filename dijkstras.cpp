d62ba8b23ff9c73d661eeb6b1523dc7ae4928f54
Lyla Zegelstein <lylazegelstein@lylas-mbp.lan>
2021-05-04 12:12:01 -0500

    Dijkstras still not done. Idk why it isn't compiling on my end

--- dijkstras.cpp
+++ dijkstras.cpp
@@ -11,94 +11,81 @@
 #include "dijkstras.h"
 //#include "dijkstras.hpp"
 #include "Airport.h"
+#include<limits.h>
+#include<float.h>
 //#include "airport.hpp"
 
 /*
- struct Airport_Node {
-     Airport* data;
-     Vertex* path;
-     bool visited;
-     
-     Airport_Node(Airport* o_data) : data(o_data), path(NULL), visited(false) {}
-     Airport_Node(Airport* o_data, Vertex* vertex) : data(o_data), path(vertex), visited(false) {}
- };
-
- struct Vertex{
-     double source_distance = DBL_MAX;
-     // double path_distance = DBL_MAX;
-     Vertex * previous = NULL;
-     Airport * data = NULL; //i think this should be a Vertex * and the source needs a previous to point at itself
-     bool visited = false;
-     
-     bool operator < (const Vertex & other) { return (source_distance < other.source_distance); }
- };
-
- */
-
-struct Vertex {
+struct Path { //iterate through these in the queues
     Airport * start;
     Airport * end;
     double weight;
+    Path(Airport * s, Airport * e, double w = DBL_MAX) : start(s), end(e), weight(w) {}
 };
 
+struct AirportNode {
+    bool visited;
+    double weight;
+    Airport * airport;
+    AirportNode(Airport * a) : visited(false), weight(DBL_MAX), airport(a) {}
+};
+*/
+
 int Dijkstras(std::map<std::string, Airport*> airportList, Airport* source) {
+
+
 /*
-    //building the graph seen in lecture
-    std::map<std::string, Airport *>::iterator it;
+               The 3 maps and there purposes:
+ 1.
+ 2.
+ 3.
+ */
+
+ /*
+    //Section 1: building the graph seen in lecture
+    std::map<std::string *, AirportNode *> allAirports;
+    std::map<std::string *, Path *> pathMap; //should create a class, and have this as a private member within the class, that way you have access to the results from Dijkstras
     
-    for (it = airportList.begin(); it != airportList.end(); it ++) {
-        unsigned long num_destinations = it->second->destinations.size();
-        for (unsigned long i = 0; i < num_destinations; i ++) {
-            Vertex vertex;
-            vertex.start = it->second;
-            vertex.end = it->second->destinations[i].other_airport;
-            vertex.weight = it->second->destinations[i].distance;
+    auto my_comp = [](const Path* p1, const Path* p2) { return p1->weight > p2->weight; };
+    std::priority_queue<Path *, std::vector<Path*>, decltype(my_comp)> pq(my_comp);
+    
+    for (auto it = airportList.begin(); it != airportList.end(); it ++) {
+        AirportNode * airport = new AirportNode(it->second);
+        allAirports.insert(std::make_pair(& airport->airport->IATA, airport));
+    }
+    
+    pq.push(new Path(NULL, source, 0)); //push the source to the priority queue
+    
+    while (! pq.empty()) {
+        Path * current = pq.top();
+        
+        pq.pop();
+        
+        AirportNode* endNode = allAirports.at(& current->end->IATA);
+        //Do we have a better path to this ending node?
+
+        if (current->weight < endNode->weight) {
+            endNode->weight = current->weight;
+            pathMap.erase(& current->end->IATA); //TODO: -> see if this creates a memory leak
+            pathMap.insert(std::make_pair(& current->end->IATA, current));
         }
+        
+        if (! endNode->visited) { //if the path hasn't been visited, you need to push destinations to pq
+            
+            for (Airport::Flights f : endNode->airport->destinations) {
+                pq.push(new Path(endNode->airport,  f.other_airport, endNode->weight + f.distance));
+            }
+            
+            endNode->visited = true;
+        }
+        //TODO: possible memory leak here, because the Path() we new() needs to get deleted
+        
     }
     */
     return 0;
 }
 
 
-
-/*
- 
- for (it = airportList.begin(); it != airportList.end(); it ++) {
-     Vertex vertex;
-     vertex.data = it->second;
-
-     if (vertex.data == source) {
-         vertex.source_distance = 0;
-         vertex.previous = &vertex;
-         pq.push(vertex);
-     }
- }
- 
- while (!pq.empty()) {
-     current = pq.top();
-     pq.pop();
-
-     if (current.visited == false) {
-         current.visited = true;
-         current.source_distance = current.previous->source_distance + current.data//got to get the distance from here
-     }
-     
-     
-     
-     double distance = current.data->destinations[i].distance + current.previous->source_distance;
-     
-     //        unsigned long num_destinations = current.data->destinations.size();
-     if (current.visited == false) {            //this means we haven't seen this node before
-         current.visited = true;
-         //mark it as visited, find the distance to the source
-         current.source_distance = current.data->destinations[i].distance + current.previous->source_distance;
-     }
-     for (auto i = 0; i < num_destinations; i ++) {
-         
-     }
- }
- 
- */
 
 
 /*
