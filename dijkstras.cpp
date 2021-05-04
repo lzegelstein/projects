a276edc8c5011f6189df5ed6c6b70642d14abbc7
Lyla Zegelstein <lylazegelstein@lylas-mbp.lan>
2021-05-04 07:04:32 -0500

    dijkstras doesn't compile yet so i commented out my progress. I am just going to build it EXACTLY how he did it in lecture and hopefully it works. I started it over bc i didn't know what to push to the priority queue

--- dijkstras.cpp
+++ dijkstras.cpp
@@ -1,62 +1,105 @@
-#include <sstream>
+/*#include <sstream>
 #include <string>
 #include <iostream>
 #include "dijkstras.h"
 #include <map>
 #include "Airport.h"
-//#include "dijstrika.hpp"
-//Dijkstra's is correct spelling -> I messed it up in the file name in xCode, my bad
+#include <sstream>
+*/
+//#include <iostream>
+#include <queue>
+#include "dijkstras.h"
+//#include "dijkstras.hpp"
+#include "Airport.h"
+//#include "airport.hpp"
 
-int Dijkstras(std::map<std::string, Airport*> airportList, Airport* source) { //TODO
-    struct Vertex {
-        double source_distance = DBL_MAX;
-        Vertex * previous = NULL;
-        Airport * data = NULL; //i think this should be a Vertex * and the source needs a previous to point at itself
-        bool visited = false;
-    };
-    
-    std::priority_queue<Vertex, std::vector<Vertex>, std::greater<double> > pq;
-//we need a vector of verticies
-    
+/*
+ struct Airport_Node {
+     Airport* data;
+     Vertex* path;
+     bool visited;
+     
+     Airport_Node(Airport* o_data) : data(o_data), path(NULL), visited(false) {}
+     Airport_Node(Airport* o_data, Vertex* vertex) : data(o_data), path(vertex), visited(false) {}
+ };
+
+ struct Vertex{
+     double source_distance = DBL_MAX;
+     // double path_distance = DBL_MAX;
+     Vertex * previous = NULL;
+     Airport * data = NULL; //i think this should be a Vertex * and the source needs a previous to point at itself
+     bool visited = false;
+     
+     bool operator < (const Vertex & other) { return (source_distance < other.source_distance); }
+ };
+
+ */
+
+struct Vertex {
+    Airport * start;
+    Airport * end;
+    double weight;
+};
+
+int Dijkstras(std::map<std::string, Airport*> airportList, Airport* source) {
+/*
+    //building the graph seen in lecture
     std::map<std::string, Airport *>::iterator it;
-    Vertex * current;
-
+    
     for (it = airportList.begin(); it != airportList.end(); it ++) {
-        Vertex vertex;
-        vertex.data = it->second;
-
-        if (vertex.data == source) {
-            vertex.source_distance = 0;
-            vertex.previous = &vertex;
-            pq.push(vertex);
+        unsigned long num_destinations = it->second->destinations.size();
+        for (unsigned long i = 0; i < num_destinations; i ++) {
+            Vertex vertex;
+            vertex.start = it->second;
+            vertex.end = it->second->destinations[i].other_airport;
+            vertex.weight = it->second->destinations[i].distance;
         }
     }
-    
-    Vertex* start;
-    start->source_distance = 0;
-    start->previous = start;
-    start->data = source;
-    pq.push(start);
-    
-    while (!pq.empty()) {
-        current = pq.top();
-        pq.pop();
-        unsigned long num_destinations = current->data->destinations.size();
-        for (auto i = 0; i < num_destinations; i ++) {
-            if (current->visited == false) {            //this means we haven't seen this node before
-                current->visited = true;
-                //mark it as visited, find the distance to the source
-                current->source_distance = current->data->destinations[i].distance + current->previous->source_distance;
-                //Now, need to see where else you can get to, and push those vertices to the pq
-                unsigned long other_vertices_inreach = current->data->destinations.size();
-                for (auto j = 0; j < other_vertices_inreach; j ++) {
-                    
-                }
-            }
-        }
-    }
+    */
     return 0;
 }
+
+
+
+/*
+ 
+ for (it = airportList.begin(); it != airportList.end(); it ++) {
+     Vertex vertex;
+     vertex.data = it->second;
+
+     if (vertex.data == source) {
+         vertex.source_distance = 0;
+         vertex.previous = &vertex;
+         pq.push(vertex);
+     }
+ }
+ 
+ while (!pq.empty()) {
+     current = pq.top();
+     pq.pop();
+
+     if (current.visited == false) {
+         current.visited = true;
+         current.source_distance = current.previous->source_distance + current.data//got to get the distance from here
+     }
+     
+     
+     
+     double distance = current.data->destinations[i].distance + current.previous->source_distance;
+     
+     //        unsigned long num_destinations = current.data->destinations.size();
+     if (current.visited == false) {            //this means we haven't seen this node before
+         current.visited = true;
+         //mark it as visited, find the distance to the source
+         current.source_distance = current.data->destinations[i].distance + current.previous->source_distance;
+     }
+     for (auto i = 0; i < num_destinations; i ++) {
+         
+     }
+ }
+ 
+ */
+
 
 /*
  Need to make sure this is not a multi edge graph.
@@ -64,38 +107,8 @@ int Dijkstras(std::map<std::string, Airport*> airportList, Airport* source) { //
  Need a visisted vector or something
  */
 
-/*
- Algorithm: I will be calculating the relative distance based on a starting point
- 
- 1. Mark every node as unvisited and set the distance to 0 for current node and infinity for the rest.
-Where to I want to keep track of what is visited and what isn't?
- Could do this as a boolean private member variable and the default is unvisited. Maybe need a more systematic way like a vector? Nope, we probably won't be going to every node.
- 
- 2. Look at the univisted airports in the destinations vector of the current and those will have the distance already calculated. Add it to the AVL Tree representing the Min Priority Queue. If the node was already visited, then compare the new distance and update it if the new distance is smaller
- 
- 3. You only mark the current cell as visited, not the neighbors!
- 
- Don't have to visit every airport, just the ones you can get to.
- 
- start at the start, and then look at only the airports you can get to, then start going through each of those and add all of the airports that they can get to and visit all of those
- 
- make a queue and start keeping track of the other airports that you need to visit
- 
- Keep track of the running paths. Maybe in an uptree? If we just keep visiting other nodes, the uptree will help find the shortest path. That way you find the shortest, you know the path to it and don't need to refind that.
- */
 
 
 
-/*
-Dijkstras::Dijkstras(std::vector<std::vector<std::string>> data) {
 
-}
-    
-std::vector<Edge> Dijkstras::Dijkstras_Helper(Vertex a, Vertex b) {
 
-}
-    
-std::vector<Edge> mainDijkstras(std::vector<std::vector<std::string>> data, Vertex a, Vertex b) {
-
-}
-*/
