4eab485137ddff49fd48569bcac53b87d7bdf729
shreesamavedhi <shreesamavedhi114@gmail.com>
2021-05-09 16:44:21 -0500

    testgraph outputs node correctly

--- tests/testGraph.cpp
+++ tests/testGraph.cpp
@@ -3,6 +3,7 @@
 #include "../traversal/AirportTraversal.h"
 #include "../traversal/DFS.h"
 #include "../Node.h"
+#include "../cs225/PNG.h"
 
 using namespace std;
 #include <iostream>
@@ -13,10 +14,10 @@ using namespace std;
 #include <algorithm>
 
 TEST_CASE("PNG output of a Node", "[Graph]") {
-    Node curr(0,0,5);
+    Node curr(100,100,15);
     cs225::PNG image;
-    image.readFromFile("../graphs/out-1.png");
-    image.resize(20,20);
+    //image.readFromFile("graphs/out-1.png");
+    image.resize(200,200);
     drawNode(curr, image);
-    image.writeToFile("../graphs/out-1.png");
+    image.writeToFile("graphs/out-2.png");
 }
