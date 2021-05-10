201279f5852b98cec4ee42f35e9a9f21fdca180e
shreesamavedhi <shreesamavedhi114@gmail.com>
2021-05-10 15:08:15 -0500

    Drawing edges works

--- tests/testGraph.cpp
+++ tests/testGraph.cpp
@@ -22,7 +22,7 @@ TEST_CASE("PNG output of a Node", "[Graph]") {
     image.writeToFile("graphs/out-2.png");
 }
 
-TEST_CASE("PNG output of two Nodes and edge (Case1)", "[Graph]") {
+TEST_CASE("PNG output of two Nodes and edge (Northwest)", "[Graph]") {
     Node one(50, 50, 2, NULL);
     Node two(100,100, 4, NULL);
     Edge line(&one, &two, 70.71);
@@ -35,7 +35,7 @@ TEST_CASE("PNG output of two Nodes and edge (Case1)", "[Graph]") {
     image.writeToFile("graphs/out-3.png");
 }
 
-TEST_CASE("PNG output of two Nodes and edge (Case2)", "[Graph]") {
+TEST_CASE("PNG output of two Nodes and edge (Southeast)", "[Graph]") {
     Node one(100, 100, 2, NULL);
     Node two(50, 50, 4, NULL);
     Edge line(&one, &two, 70.71);
@@ -48,7 +48,7 @@ TEST_CASE("PNG output of two Nodes and edge (Case2)", "[Graph]") {
     image.writeToFile("graphs/out-4.png");
 }
 
-TEST_CASE("PNG output of two Nodes and edge (Case3)", "[Graph]") {
+TEST_CASE("PNG output of two Nodes and edge (Horizontal)", "[Graph]") {
     Node one(50, 100, 2, NULL);
     Node two(100, 100, 4, NULL);
     Edge line(&one, &two, 70.71);
@@ -61,7 +61,7 @@ TEST_CASE("PNG output of two Nodes and edge (Case3)", "[Graph]") {
     image.writeToFile("graphs/out-5.png");
 }
 
-TEST_CASE("PNG output of two Nodes and edge (Case4)", "[Graph]") {
+TEST_CASE("PNG output of two Nodes and edge (Vertical)", "[Graph]") {
     Node one(100, 100, 2, NULL);
     Node two(100, 50, 4, NULL);
     Edge line(&one, &two, 70.71);
@@ -74,7 +74,7 @@ TEST_CASE("PNG output of two Nodes and edge (Case4)", "[Graph]") {
     image.writeToFile("graphs/out-6.png");
 }
 
-TEST_CASE("PNG output of two Nodes and edge (Case5)", "[Graph]") {
+TEST_CASE("PNG output of two Nodes and edge (Northeast)", "[Graph]") {
     Node one(100, 50, 2, NULL);
     Node two(50, 100, 4, NULL);
     Edge line(&one, &two, 70.71);
@@ -87,7 +87,7 @@ TEST_CASE("PNG output of two Nodes and edge (Case5)", "[Graph]") {
     image.writeToFile("graphs/out-7.png");
 }
 
-TEST_CASE("PNG output of two Nodes and edge (Case6)", "[Graph]") {
+TEST_CASE("PNG output of two Nodes and edge (Southwest)", "[Graph]") {
     Node one(50, 100, 2, NULL);
     Node two(100, 50, 4, NULL);
     Edge line(&one, &two, 70.71);
@@ -98,4 +98,43 @@ TEST_CASE("PNG output of two Nodes and edge (Case6)", "[Graph]") {
 
     drawEdge(line, image);
     image.writeToFile("graphs/out-8.png");
+}
+
+TEST_CASE("PNG output of two Nodes and edge (Edge case: not quite horizontal)", "[Graph]") {
+    Node one(50, 100, 2, NULL);
+    Node two(100, 99, 4, NULL);
+    Edge line(&one, &two, 70.71);
+    cs225::PNG image;
+    image.resize(500, 500);
+    drawNode(one, image);
+    drawNode(two, image);
+
+    drawEdge(line, image);
+    image.writeToFile("graphs/out-9.png");
+}
+
+TEST_CASE("PNG output of two Nodes and edge (Edge case: not quite vertical)", "[Graph]") {
+    Node one(50, 100, 2, NULL);
+    Node two(49, 50, 4, NULL);
+    Edge line(&one, &two, 70.71);
+    cs225::PNG image;
+    image.resize(500, 500);
+    drawNode(one, image);
+    drawNode(two, image);
+
+    drawEdge(line, image);
+    image.writeToFile("graphs/out-10.png");
+}
+
+TEST_CASE("PNG output of two Nodes and edge (Edge case: not quite Northeast)", "[Graph]") {
+    Node one(100, 50, 2, NULL);
+    Node two(49, 100, 4, NULL);
+    Edge line(&one, &two, 70.71);
+    cs225::PNG image;
+    image.resize(500, 500);
+    drawNode(one, image);
+    drawNode(two, image);
+
+    drawEdge(line, image);
+    image.writeToFile("graphs/out-11.png");
 }
