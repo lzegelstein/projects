c3beef550ecc6d5e4325006e4d83f4170f05685f
Jared Frank <jafrank2@illinois.edu>
2021-05-09 23:01:26 -0500

    makefile work

--- main.cpp
+++ main.cpp
@@ -6,38 +6,7 @@
 #include <cstdlib>
 #include "AirTravel.h"
 #include "Airport.h"
-/*
- Questions for mentor meeting:
- 1. Is there a way to get around having to do AirTravel:: like in line for loop of findBusiestAirport()?
- Make airport struct external, it can still be in the same file.
- Don't template it
- 
- 
- Mazes:
- We get the distance easily from the BFS, #of steps. The end of the maze is defined as the square on the bottom that is the furthest.
- 
- Do BFS of the whole thing as a traversal and at the end, you'll know the distance traveled. Do't do BFS to a point.
- BFS finds the endpoint by computing the distance.
- Along the way, save what square you came from
- */
 
-/* NEXT STEPS:
-1. Fixing the runtime of BusiestAirport(). Right now it is O(N) but we will continuously update it as the destination vector is filled in. That is a task for not today though
-2. Complete the algorithms
-3. Get a graphic output
- */
-
-/*
- Mentor meeting:
- Issue with Distrikya's -> how do you know where a "superior" vertex is?
- It is difficult to know how to handle the priority code -> next group session we should discuss this
- If we knew where it was, we could check to see if it was too big or too small and then heapify accordingly
- One way to get around it is to used a pointer based structure
- Another solution is to put extra nodes in but that compromises runtime sounds complex, do the other one
- 
- 
- */
-//"/Users/lylazegelstein/Desktop/airportnames.csv"
 int main() {
     std::cout << "AirTravel application started" << std::endl;
     AirTravel ourmap("data/airports-short.csv", "data/routes-short.csv");
@@ -54,6 +23,7 @@ int main() {
     return 0;
 }
 
+//Previos Main 
 /*
  std::string starting_point("ORD");
  std::string ending_point("JFK");
