451d63f367407806c570ccb0c016a72080f13552
Lyla Zegelstein <lylazegelstein@lylas-mbp.lan>
2021-05-11 13:42:35 -0500

    made a user friendly main

--- main.cpp
+++ main.cpp
@@ -25,14 +25,14 @@
    
 //AirTravel ourmap("data/airports-short.csv", "data/routes-short.csv");
 
-void dfs_print(DFS * stuff);
+void dfs_print(AirportTraversal::Iterator it, DFS dfs);
 
-void dfs_print(DFS * stuff) {
-    while (!stuff->empty()) {
-        std::cout<<stuff->pop().name<<std::endl;
+void dfs_print(AirportTraversal::Iterator it, DFS dfs) {
+    while (it != dfs.end()) {
+        std::cout<<(*it).name<<std::endl;
+        ++ it;
     }
 }
- 
 int main(int argc, char** argv) {
    std::cout << "AirTravel application started" << std::endl;
   
@@ -48,25 +48,22 @@ int main(int argc, char** argv) {
    */
 
   //DFS
-    AirTravel dfsAlg("data/airport.csv", "data/routes.csv");
-    std::string dfs_source_airport("MHC");
+  /*
+    AirTravel dfsAlg("/Users/lylazegelstein/Desktop/airports-short.csv", "/Users/lylazegelstein/Desktop/routes-short.csv");
+    std::string dfs_source_airport("ATL");
     //std::string dfs_destination_airport("ULK");
     Airport* dfs_source = dfsAlg.IATAsearch(dfs_source_airport);
     DFS * dfs_ptr = dfsAlg.DepthFirstSearch(dfs_source);
-    dfs_print(dfs_ptr);
+    dfs_print(dfs_ptr->begin(), *dfs_ptr);
+  */
     
-
-//Airport* destination = dfsAlg.IATAsearch(dfs_destination_airport);
-
-  //DFS* DepthFirstSearch(Airport* source);
-
   //Graphic Output
-  /*
+  
     AirTravel ourmap("data/airport.csv", "data/routes.csv");
     Graph * gr = ourmap.worldMap(4000, 2000);
     cs225::PNG * png_ptr = gr->makeImage();
     png_ptr->writeToFile("out-ourGraph.png");
-  */
+  
 
 
     std::cout << "AirTravel application ended" << std::endl;
@@ -74,14 +71,4 @@ int main(int argc, char** argv) {
 }
 
 
-/*
-    Graph * gr = ourmap.worldMap(4000, 2000);
-    cs225::PNG * png_ptr = gr->makeImage();
 
-    png_ptr->writeToFile("out-ourGraph.png");
-
-    Airport* lyr = ourmap.IATAsearch("LYR");
-
-    std::cout << lyr->name << std::endl;
-
-*/
